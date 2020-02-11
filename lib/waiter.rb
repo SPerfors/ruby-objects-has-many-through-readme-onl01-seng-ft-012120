1a3ec84288485b240e1ee0ee7ee0f29cbfe1d3b4
Steven Perfors <stevecanbuild@gmail.com>
2020-02-11 16:07:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,5 +13,21 @@ class Waiter
   def self.all
     @@all
   end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+ 
+  def waiters
+    meals.map do |meal|
+      meal.waiter
+    end
+  end
+ 
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
  
 end
