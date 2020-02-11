e922671d8cf7098c2954339400cf2c496d8098d8
Steven Perfors <stevecanbuild@gmail.com>
2020-02-11 16:08:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -20,12 +20,13 @@ class Waiter
     end
   end
  
-  def waiters
-    meals.map do |meal|
-      meal.waiter
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
     end
+    best_tipped_meal.customer
   end
- 
+  
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
